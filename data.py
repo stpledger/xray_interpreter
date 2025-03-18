97bf1df9f6ab9109b52542f84875be2f15e0f2f8
Scott Pledger <stpledger@gmail.com>
2025-03-17 21:39:54 -0500

    dataloader configured

--- data.py
+++ data.py
@@ -2,31 +2,42 @@ from pathlib import Path
 
 import torch
 from PIL import Image
+import pandas as pd
 
-DATASET_PATH = Path(__file__).parent.parent / "data"
+IMAGES_PATH = Path('/Users/stple/Documents/Projects_Local/xray_interpreter/data')
 
 
 class ImageDataset:
     image_paths: list[Path]
     _image_cache: list[torch.Tensor | None]
     _cache_images: bool
+    labels: dict[str, int]  # Adjust the label type as needed
 
     def __init__(self, split: str, cache_images: bool = True):
-        self.image_paths = list((DATASET_PATH / split).rglob("*.jpg"))
+        # Create a list of image paths that have a corresponding label.
+        labels_csv = pd.read_csv(f"{split}.csv", index_col=0).drop(columns=['Patient ID'])
+        self.labels = labels_csv.apply(lambda row: row.tolist(), axis=1).to_dict()
+
+        all_paths = list((IMAGES_PATH).rglob("*.png"))
+        self.image_paths = [p for p in all_paths if p.name in self.labels]
         self._image_cache = [None] * len(self.image_paths)
         self._cache_images = cache_images
 
     def __len__(self) -> int:
         return len(self.image_paths)
 
-    def __getitem__(self, idx: int) -> torch.Tensor:
+    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
         import numpy as np
 
-        cached_image = self._image_cache[idx]
-        if cached_image is not None:
-            return cached_image
-
-        img = torch.tensor(np.array(Image.open(self.image_paths[idx])), dtype=torch.uint8)
         if self._cache_images:
-            self._image_cache[idx] = img
-        return img
+            cached_image = self._image_cache[idx]
+            if cached_image is not None:
+                img = cached_image
+        else:
+            img = torch.tensor(np.array(Image.open(self.image_paths[idx])), dtype=torch.uint8)
+            if self._cache_images:
+                self._image_cache[idx] = img
+
+        # Retrieve the label using the image file name
+        label = torch.Tensor(self.labels[self.image_paths[idx].name])
+        return img, label

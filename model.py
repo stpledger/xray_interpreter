b06dff7c56c94f87cbab12347b9183a82129c224
Scott Pledger <stpledger@gmail.com>
2025-03-23 20:40:11 -0500

    efficient net b4 option added

--- model.py
+++ model.py
@@ -1,11 +1,11 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
-from torchvision.models import EfficientNet_B1_Weights, EfficientNet_B6_Weights
 
 class EfficientNetB1(nn.Module):
     def __init__(self, num_classes: int = 1000, fresh: bool = False):
         super(EfficientNetB1, self).__init__()
+        from torchvision.models import EfficientNet_B1_Weights
         if fresh:
             self.model = models.efficientnet_b1(weights=None)
         else:
@@ -19,13 +19,31 @@ class EfficientNetB1(nn.Module):
         return self.model(x), {}
     
 
+class EfficientNetB4(nn.Module):
+    def __init__(self, num_classes: int = 1000, fresh: bool = False):
+        super(EfficientNetB4, self).__init__()
+        from torchvision.models import EfficientNet_B4_Weights
+        if fresh:
+            self.model = models.efficientnet_b4(weights=None)
+        else:
+            self.model = models.efficientnet_b4(weights=EfficientNet_B4_Weights.DEFAULT)
+            
+        if num_classes != 1000:
+            in_features = self.model.classifier[1].in_features
+            self.model.classifier[1] = nn.Linear(in_features, num_classes)
+
+    def forward(self, x):
+        return self.model(x), {}
+    
+
 class EfficientNetB6(nn.Module):
     def __init__(self, num_classes: int = 1000, fresh: bool = False):
         super(EfficientNetB6, self).__init__()
+        from torchvision.models import EfficientNet_B6_Weights
         if fresh:
-            self.model = models.efficientnet_b1(weights=None)
+            self.model = models.efficientnet_b6(weights=None)
         else:
-            self.model = models.efficientnet_b1(weights=EfficientNet_B6_Weights.DEFAULT)
+            self.model = models.efficientnet_b6(weights=EfficientNet_B6_Weights.DEFAULT)
             
         if num_classes != 1000:
             in_features = self.model.classifier[1].in_features
